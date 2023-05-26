456a7c706d0ba529da21ce9597911f93ea4007c2
Girish1369 <girishkatkamwar.1369@gmail.com>
2023-05-26 19:21:18 +0530

    addtion of two number at local

---
+++ src/com/demo/additioninlocal.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class additioninlocal {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		
+		System.out.println("addtion="+(a+b));
+		
+
+	}
+
+}
